a8a0e404bc335c96f28921fa01cd154466ce9db7
buiphihien24032001 <buiphihien24032001@gmail.com>
2021-06-03 01:08:00 +0700

    first commit

---
+++ QuanLyPet/src/JDBC/Connec.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JDBC;
+ import java.sql.*;
+/**
+ *
+ * @author Admin
+ */
+public class Connec {
+    String user = "sa";
+    String pass = "123";
+    String url = "jdbc:sqlserver://localhost:1433;databaseName=QuanLyPet";
+    
+    
+    
+}
